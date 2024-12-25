3e7074f0f70e2cf1a53a1dbbbfbfc1300d984b96
GeorgeKnyazkov <weemercury009@yandex.ru>
2024-12-25 21:55:25 +0300

    added topics.html with urls and views

--- learning_logs_apps/urls.py
+++ learning_logs_apps/urls.py
@@ -7,5 +7,8 @@ from . import views
 app_name = 'learning_logs_apps'
 urlpatterns = [
     # Домашняя страница
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # Страница со списком всех тем.
+    path('topics/', views.topics, name='topics'),
+    # Страница с подробной информацией по отдельной теме
 ]
--- learning_logs_apps/views.py
+++ learning_logs_apps/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 
 def index(request):
     """Домашняя страница приложения learning_logs_apps"""
     return render(request, 'learning_logs_apps/index.html')
+
+def topics(request):
+    """Выводит список тем."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs_apps/topics.html', context)
