82817ad409527e77d472d75d08c87a28c18fa7f4
GeorgeKnyazkov <weemercury009@yandex.ru>
2024-12-25 22:40:38 +0300

    added topic.html with urls and views

--- learning_logs_apps/urls.py
+++ learning_logs_apps/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     # Страница со списком всех тем.
     path('topics/', views.topics, name='topics'),
     # Страница с подробной информацией по отдельной теме
+    path('topics/<int:topic_id>', views.topic, name='topic')
 ]
