284e71a5c349b35178b1cb916163b6608148f2a5
GeorgeKnyazkov <weemercury009@yandex.ru>
2024-12-26 23:09:00 +0300

    added restricting access to data

--- learning_log_project/settings.py
+++ learning_log_project/settings.py
@@ -125,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Мои настройи
+LOGIN_URL = '/users/login/' 
--- learning_logs_apps/views.py
+++ learning_logs_apps/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -9,19 +11,26 @@ def index(request):
     """Домашняя страница приложения learning_logs_apps"""
     return render(request, 'learning_logs_apps/index.html')
 
+@login_required
 def topics(request):
     """Выводит список тем."""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs_apps/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """Выводит одну тему и все ее записи."""
     topic = Topic.objects.get(id=topic_id)
+    # Проверка принадлежности темы текущему пользователю.
+    if topic.owner != request.user:
+        raise Http404
+    
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs_apps/topic.html', context)
 
+@login_required
 def new_topic(request):
     """Определяет новую тему."""
     if request.method != 'POST':
@@ -31,16 +40,23 @@ def new_topic(request):
         # Отправлены данные POST - обработать данные.
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs_apps:topics')
         
     # Вывести пустую или недействительную форму.
     context = {'form': form}
     return render(request, 'learning_logs_apps/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """Добавляет новую запись по конкретной теме."""
     topic = Topic.objects.get(id=topic_id)
+                
+    if topic.owner != request.user:
+        raise Http404
+            
     if request.method != 'POST':
         # Данные не отправлялись, создается пустая форма.
         form = EntryForm()
@@ -57,10 +73,14 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs_apps/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """Редактирует существующую запись."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    
+    if topic.owner != request.user:
+        raise Http404
     
     if request.method != 'POST':
         # Исходный запрос, создается пустая форма.
