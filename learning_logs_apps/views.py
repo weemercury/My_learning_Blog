e7b36c386d267a1ddfdad0d0056fb59bcd444248
GeorgeKnyazkov <weemercury009@yandex.ru>
2024-12-26 15:29:05 +0300

    added new form to add new topic

--- learning_logs_apps/views.py
+++ learning_logs_apps/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 from .models import Topic
+from .forms import TopicForm
 
 # Create your views here.
 
@@ -19,3 +21,19 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs_apps/topic.html', context)
+
+def new_topic(request):
+    """Определяет новую тему."""
+    if request.method != 'POST':
+        # Данные не отправлялись, создается пустая форма.
+        form = TopicForm()
+    else:
+        # Отправлены данные POST - обработать данные.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs_apps:topics')
+        
+    # Вывести пустую или недействительную форму.
+    context = {'form': form}
+    return render(request, 'learning_logs_apps/new_topic.html', context)
