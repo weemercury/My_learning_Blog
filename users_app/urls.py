4dd025a70d7ef360e304c25820428b3e0bca7e52
GeorgeKnyazkov <weemercury009@yandex.ru>
2024-12-26 18:57:40 +0300

    added page for registration new user

--- users_app/urls.py
+++ users_app/urls.py
@@ -2,8 +2,12 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users_app'
 urlpatterns = [
     # Включить URL авторизации по умолчанию.
-    path('', include('django.contrib.auth.urls'))
+    path('', include('django.contrib.auth.urls')),
+    # Страница регистрации.
+    path('register/', views.register, name='register'),
 ]
